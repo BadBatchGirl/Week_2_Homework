91974e00c0823603d31e50f8aabd30f1b4c21d8d
Tamika Webb <94010537+BadBatchGirl@users.noreply.github.com>
2021-11-16 17:15:53 -0500

    Create week_2homework.py
    I need to learn how to round integers. Will focus on it later this week.

---
+++ week_2homework.py
@@ -0,0 +1,34 @@
+#Exercise 1: Fahrenheit to Celsius
+#prompt your user for the degrees in Fahrenheit
+#Display the degrees in Fahrenheit and its equivalent in Celsius on the Console
+# num1 = int(input("Please enter degrees in Fahrenheit: ")) 
+# num2 = -(num1-32) * 5/9
+# print(f"{num1}°F")
+# print(f"{num2}°C")
+
+#Exercise 2: Sales Tax
+# Prompt your user for the price of their product before taxes
+# Prompt the user to give you the sales tax
+# Price = float(input("Please enter the price of the item: $"))
+# Sales_tax = float(input("Please enter the sales tax: "))
+# Percent = Sales_tax / 100 
+# final_price = Price * Percent + Sales_tax
+# print(f"${final_price}")
+
+# Exercise 3: Tip Calculator
+# Prompt the user to give you the total bill
+# Prompt the user to give you what percent they would like to tip
+# Display to the user the amount of tip they would owe
+# total_bill = float(input("Please enter the amount of the total bill: $"))
+# tip = float(input("What percent would you like to tip? "))
+# tip_amount = total_bill * tip/100
+# print(tip_amount)
+
+# Exercise 4: Bill Dividends
+# Prompt the user to give you the total bill
+# Prompt the user to give you how many people are contributing
+# Display to the user, the amount each contributor would need to pay
+# total_bill = float(input("Please enter the amount of the total bill: $"))
+# people = int(input("How many people are contributing?'"))
+# amount_per_person = total_bill / people
+# print(f"${amount_per_person}")
